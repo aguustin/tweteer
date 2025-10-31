d94c6d0f67fa2b78ce617cb8dbc1529bc5c155b9
Agustin Mole <amole@godoycruz.gov.ar>
2025-10-31 10:11:36 -0300

    .

--- client/src/api/userRequests.jsx
+++ client/src/api/userRequests.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export const getAllUsersRequest = async () => axios.get('http://localhost:8080/getAllUsers');
+export const getAllUsersRequest = async () => axios.get('https://tweteer.onrender.com/getAllUsers');
 
-export const createUserRequest = async (accountData) =>  axios.post('http://localhost:8080/createUser', accountData);
+export const createUserRequest = async (accountData) =>  axios.post('https://tweteer.onrender.com/createUser', accountData);
 
-export const authenticateUserRequest = async (authenticateData) => axios.post('http://localhost:8080/authenticateUser', authenticateData);
+export const authenticateUserRequest = async (authenticateData) => axios.post('https://tweteer.onrender.com/authenticateUser', authenticateData);
 
-export const editPasswordRequest = async (userName, password, confirmPassword) => axios.put(`http://localhost:8080/editPassword/${userName}/${password}/${confirmPassword}`);
+export const editPasswordRequest = async (userName, password, confirmPassword) => axios.put(`https://tweteer.onrender.com/editPassword/${userName}/${password}/${confirmPassword}`);
 
 export const editProfileRequest = async (editData) => {
     const form = new FormData();
@@ -15,15 +15,15 @@ export const editProfileRequest = async (editData) => {
         form.append(key, editData[key]);
     }
 
-    return await axios.post('http://localhost:8080/editProfile', form, {
+    return await axios.post('https://tweteer.onrender.com/editProfile', form, {
         headers:{
             "Content-Type": "multipart/form-data",
         },
     });
 }
 
-export const followRequest = async (followingId, sessionId) => axios.put(`http://localhost:8080/following/${followingId}/${sessionId}`);
+export const followRequest = async (followingId, sessionId) => axios.put(`https://tweteer.onrender.com/following/${followingId}/${sessionId}`);
 
-export const checkFollowRequest = async (followingId) => axios.get(`http://localhost:8080/checkFollow/${followingId}`);
+export const checkFollowRequest = async (followingId) => axios.get(`https://tweteer.onrender.com/checkFollow/${followingId}`);
 
-export const unFollowRequest = async (followingId, sessionId) => axios.put(`http://localhost:8080/unFollow/${followingId}/${sessionId}`);
+export const unFollowRequest = async (followingId, sessionId) => axios.put(`https://tweteer.onrender.com/unFollow/${followingId}/${sessionId}`);
