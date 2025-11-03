7de67baf6c0866f46a274b76123ae6f41c4dfe93
Agustin Mole <amole@godoycruz.gov.ar>
2025-11-03 09:25:42 -0300

    .

--- server/config.js
+++ server/config.js
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const port = process.env.PORT || 4000;
-export const mongoose_uri = process.env.MONGO_URI_DEV;
+export const mongoose_uri = process.env.MONGO_URI;
